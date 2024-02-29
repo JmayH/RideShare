d6c1efb20a0fa60bab67a6248a15af69189f26de
Jamie Hsieh <jhsieh26@students.polytechnic.org>
2024-02-28 21:43:04 -0800

    Made Passenger.java

---
+++ Passenger.java
@@ -0,0 +1,23 @@
+public class Passenger
+{
+
+    private int destination;
+    
+    public Passenger(int destination)
+    {
+
+        this.destination = destination;
+
+       
+
+    }
+
+    public int getDestination()
+    {
+        return destination;
+    }
+
+   
+
+
+}
